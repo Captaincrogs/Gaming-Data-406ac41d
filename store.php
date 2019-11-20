2f8aa81d3bf62a54877856e916007fc5d3f6e29e
Captaincrogs <2003829@talnet.nl>
2019-11-20 12:25:25 +0100

    Uitwerking van de exercise

---
+++ store.php
@@ -0,0 +1,18 @@
+<?php
+
+$Games = array ( 
+
+ "schietspellen" =>
+     array("Call of Duty" , "battlefield"),
+ 
+ "avontuur" =>
+     array("minecraft" , "headspun" , "resident evil"),
+ 
+ "singleplayer" =>
+     array("Stardew Valley" , "far_cry" , "batman" , "Gears of War Ultimate Edition"),
+ 
+ "MMORPG" =>
+    array("diablo" , "worldofwarcraft" ));
+	
+	echo $Games["singleplayer"][0];
+	
